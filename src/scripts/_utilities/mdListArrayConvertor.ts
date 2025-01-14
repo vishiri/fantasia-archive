dad3c57f68b5795f09a8602f72b070c125cabe01
Vishiri Rilgaran <vishiri.rilgatan@gmail.com>
2025-01-14 23:08:48 +0100

    adjusted MD files list convertor to also remove non-list lines in the file if they are present

--- src/scripts/_utilities/mdListArrayConvertor.ts
+++ src/scripts/_utilities/mdListArrayConvertor.ts
@@ -1,13 +1,19 @@
 export function mdListArrayConvertor (mdString: string) {
+  // Remove all lines that do not start with "- "
+  mdString = mdString.replaceAll(/^(?!- ).*$/gm, '')
+
+  // Remove all empty lines and leading/trailing whitespace
+  mdString = mdString.replaceAll(/^\s*[\r\n]/gm, '')
+
   // Remove leading "- "
-  let removedLeads = mdString.replaceAll('\n- ', '\n')
-  if (removedLeads.substring(0, 2) === '- ') { removedLeads = removedLeads.slice(2) }
+  mdString = mdString.replaceAll('\n- ', '\n')
 
   // Cleaned up ` characters and replace them with "
-  const cleanedString = removedLeads.replaceAll('`', '"')
+  mdString = mdString.replaceAll('`', '"')
 
   // Split the list by leading newline characters and remove the last empty line (if present)
-  const tipArray = cleanedString.split('\n')
+  const tipArray = mdString.split('\n')
   if (tipArray[tipArray.length - 1] === '') { tipArray.pop() }
+
   return tipArray
 }
