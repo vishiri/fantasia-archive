90bfb3d5abf13220845a26c751b4e024e5a269cf
Vishiri Rilgaran <vishiri.rilgatan@gmail.com>
2025-02-18 23:18:49 +0100

    added spellecheck on right click

--- src-electron/mainScripts/mainWindowCreation.ts
+++ src-electron/mainScripts/mainWindowCreation.ts
@@ -1,4 +1,4 @@
-import { BrowserWindow, app, screen } from 'electron'
+import { BrowserWindow, app, screen, MenuItem, Menu, shell } from 'electron'
 import { enable } from '@electron/remote/main'
 import path from 'path'
 
@@ -91,4 +91,31 @@ export const mainWindowCreation = () => {
 
   // Check if we are on the primary or secondary instance of the app
   preventSecondaryAppInstance(appWindow)
+
+  appWindow.webContents.on('context-menu', (_, params) => {
+    if (appWindow === undefined) { return }
+    const menu = new Menu()
+    const webContents = appWindow.webContents
+
+    // Add each spelling suggestion
+    for (const suggestion of params.dictionarySuggestions) {
+      menu.append(new MenuItem({
+        label: suggestion,
+        click: () => webContents.replaceMisspelling(suggestion)
+      }))
+    }
+
+    // Allow users to add the misspelled word to the dictionary
+    if (params.misspelledWord) {
+      menu.append(
+        new MenuItem({
+          label: 'Add to dictionary',
+          click: () => webContents.session.addWordToSpellCheckerDictionary(params.misspelledWord)
+        })
+      )
+    }
+    if ((params.dictionarySuggestions && params.dictionarySuggestions.length) || params.misspelledWord) {
+      menu.popup()
+    }
+  })
 }
--- src/boot/externalLinkManager.ts
+++ src/boot/externalLinkManager.ts
@@ -1,41 +1,58 @@
 import { boot } from 'quasar/wrappers'
 
-export default boot(() => {
-  document.addEventListener('click', e => {
+const handleMouseClicks = (event: MouseEvent) => {
+  /**
+   * Target of the click event
+   */
+  const clickTarget = event.target as HTMLAnchorElement
+
+  if (clickTarget === null) { return }
+
+  /**
+   * Prepare the actual link target variable
+   */
+  let targetLink: HTMLAnchorElement|false = (clickTarget.tagName === 'a')
+    ? clickTarget
+    : false
+
+  if (targetLink === false) {
+    const parentLink: HTMLAnchorElement|null = clickTarget.closest('a')
+    targetLink = (parentLink !== null)
+      ? parentLink
+      : false
+  }
+
+  if (targetLink === false) { return }
+
+  /**
+   * HREF link of the url
+   */
+  const linkURL = targetLink.href
+
+  /**
+   * Determines if the URL is extenal or not
+   */
+  const isExternal = window.faContentBridgeAPIs.faExternalLinksManager.checkIfExternal(linkURL)
+
+  if (isExternal) {
     /**
-     * Target of the click event
+     * If the URL is external, prevent default and open the URL via the electron API functionality
      */
-    const clickTarget = e.target as HTMLAnchorElement
+    event.preventDefault()
+    window.faContentBridgeAPIs.faExternalLinksManager.openExternal(linkURL)
+  }
 
-    if (clickTarget) {
-      /**
-       * Closest anchor element
-       * Selector might end up being empty if the anchor itself was clicked. If so, select it instead
-       */
-      let originLink: HTMLAnchorElement|null|false = clickTarget.closest('a')
-      if (originLink === null) {
-        originLink = (clickTarget.tagName === 'a') ? clickTarget : false
-      }
+  if (event.type === 'auxclick') {
+    event.preventDefault()
 
-      if (originLink) {
-        /**
-         * HREF link of the url
-         */
-        const linkURL = originLink.href
-
-        /**
-         * Determines if the URL is extenal or not
-         */
-        const isExternal = window.faContentBridgeAPIs.faExternalLinksManager.checkIfExternal(linkURL)
-
-        if (isExternal) {
-          /**
-           * If the URL is external, prevent default and open the URL via the electron API functionality
-           */
-          e.preventDefault()
-          window.faContentBridgeAPIs.faExternalLinksManager.openExternal(linkURL)
-        }
-      }
+    if (!isExternal) {
+      window.location.href = linkURL
     }
-  })
+  }
+}
+
+export default boot(() => {
+  document.addEventListener('click', handleMouseClicks)
+
+  document.addEventListener('auxclick', handleMouseClicks)
 })
